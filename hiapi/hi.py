55ed2f6baffb420d2b7a3dd0dfc3012786395b75
Ryan Jung <ryan.jung@rackspace.com>
2015-01-22 21:11:22 -0600

    Added main function

---
+++ hiapi/hi.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello, world!\n'
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-b', '--bind-address',
+                        dest='bind', default='127.0.0.1')
+    parser.add_argument('-p', '--port', dest='port', default=4000, type=int)
+    return parser.parse_args()
+
+def main():
+    opts = parse_args()
+    app.run(host=opts.bind, port=opts.port)
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,24 @@
+"""Set up hiapi
+"""
+
+from distutils.core import setup
+
+setup(
+    name='pyhiapi',
+    version='0.1',
+    author='Ryan Jung',
+    author_email='gradysghost@gmail.com',
+    package=['pyhiapi'],
+    url='https://github.com/GradysGhost/pyhiapi',
+    license='LICENCE',
+    description='PyHiAPI - Simple test API',
+    long_description=open('README.md').read(),
+    install_requires=[
+        'flask'
+    ],
+    entry_points={
+        'console_scripts': [
+            'hiapi = hiapi.hi:main'
+        ]
+    }
+)
