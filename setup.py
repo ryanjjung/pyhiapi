282a81c0b9a93b92f7f9d992bdd53f4af13f95f9
ryanjjung <ryanjjung@gmail.com>
2021-07-14 11:31:27 -0600

    Move README file, bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
     name='pyhiapi',
-    version='0.1.4',
+    version='0.2.0',
     author='Ryan Jung',
     author_email='ryanjjung@gmail.com',
     packages=['hiapi'],
